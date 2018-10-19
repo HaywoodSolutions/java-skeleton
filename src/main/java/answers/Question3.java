d539487eacb3bc6f66bfe206ea0acc84a4f14680
Thomas Haywood <thomashaywoodproductions@gmail.com>
2018-10-19 18:09:50 +0100

    Question 3

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -5,20 +5,29 @@ import helpers.Edge;
 public class Question3 {
 
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
-		int[] legs = new int[numNodes];
-        for (int e=0; e<edgeList.length; e++) {
-            legs[edgeList[e].getEdgeA()]++;
-            legs[edgeList[e].getEdgeB()]++;
+		int[] nodes = new int[numNodes];
+        for (int e=0; e < edgeList.length; e++) {
+            nodes[edgeList[e].getEdgeA() - 1]++;
+            nodes[edgeList[e].getEdgeB() - 1]++;
         }
         
-        int total = 0;
-        for (int l=0; l<legs.length; l++) {
-            if (legs[l] == numNodes - 1) {
-                total += 1;
-            } else total -= 1;
+        int[] legs = new int[numNodes];
+        for (int n=0; n<nodes.length; n++) {
+            legs[nodes[n]]++;
         }
-          
-		return total;
+        
+        boolean first = true;
+        int total = 0;
+        for (int l= legs.length - 1; l >= 0; l--) {
+            if (legs[l] > 0)  {
+                System.out.println(legs[l]);
+                if (first) {
+                    total -= legs[l];
+                    first = false;
+                } else total += legs[l];
+            }
+        }
+        return total;
 	}
 
 }
