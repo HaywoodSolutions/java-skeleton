48409f75eb53cfa89215bdaabe80779c3117bbda
Thomas Haywood <thomashaywoodproductions@gmail.com>
2018-10-19 14:21:34 +0100

    Question 1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,31 @@
 package answers;
 
 public class Question1 {
-
-	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
-
+    public static int bestMergedPortfolio(int[] portfolios) {
+        ArrayList portfoliosList = new ArrayList<Integer>(100);
+        int BitMaxInt = (int) Math.pow(2, 16) - 1;
+        int max = 0;
+        for (int i=0; i < portfolios.length; i++) {
+            int currentPort = portfolios[i];
+            if (currentPort <= BitMaxInt && !portfoliosList.contains(currentPort)) {
+                for(Object port : portfoliosList) {
+                    int total = addWOverflow(((int) port), currentPort);
+                    if (max < total) {
+                        max = total;
+                    }
+                }
+            }
+            portfoliosList.add(currentPort);
+        }
+        return max > 0 ? max : -1;
+    }
+    
+    public static String toBinary(Integer portfolioInt) {
+      return Integer.toString(portfolioInt, 2);
+    }
+    
+    public static int addWOverflow(int i1, int i2) {
+        int sum = (i1 + i2) % 65535;
+        return sum;
+    }
 }
