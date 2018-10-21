099ce9054a336e39343f0afefe5348b8921ee3a6
Thomas Haywood <thomashaywoodproductions@gmail.com>
2018-10-21 14:33:01 +0100

    Q5

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -3,8 +3,35 @@ package answers;
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		// TODO Auto-generated method stub
-		return -1;
+		int[] nodes = new int[allowedAllocations.length];
+        int[] newNodes;
+        int newNodesLength = allowedAllocations.length;
+        int pass = 1;
+        
+        for (int i=0; i<allowedAllocations.length; i++) {
+            if (totalValue == allowedAllocations[i]) {
+                return pass;
+            } else if (allowedAllocations[i] < totalValue) 
+                nodes[i] = allowedAllocations[i];
+        }
+        while (nodes.length > 0) {
+            newNodes = new int[(newNodesLength * allowedAllocations.length)];
+            newNodesLength = 0;
+            for (int a=0; a<nodes.length; a++) {
+                for (int b=0; b<allowedAllocations.length; b++) {
+                    int total = nodes[a] + allowedAllocations[b];
+                    if (total == totalValue) {
+                        return pass + 1;
+                    } else if (total < totalValue) {
+                        newNodes[newNodesLength] = nodes[a] + allowedAllocations[b];
+                        newNodesLength++;
+                    }
+                }
+            }
+            nodes = newNodes;
+            pass++;
+        }
+        return -1;
 	}
 
 }
