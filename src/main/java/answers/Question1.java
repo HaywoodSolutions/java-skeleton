8c43e24810ecb1bd4cfea23dfdfd0842923532dd
Unknown <thomashaywoodproductions@gmail.com>
2018-10-21 22:15:05 +0100

    Q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -6,40 +6,6 @@ import java.util.List;
 public class Question1 {
     
     public static int bestMergedPortfolio(int[] portfolios) {
-        int [][] finalPortfolio = toBinary(portfolios);
-        
-        // Merge all the portfolios with another
-        List<Integer> binaryList = new ArrayList();
-        List<Integer> finalList = new ArrayList();
-        for (int z = 0; z <= portfolios.length - 1; z++) {
-            for (int y = portfolios.length - 1; y >= 0; y--) {
-                finalList.addAll(mergePortfolios(finalPortfolio[z], finalPortfolio[y], binaryList));
-                binaryList.clear();
-            }
-        }
-        int indexMerge = (int) Math.sqrt(portfolios.length - 1);
-        
-        // Modify the finalList to return every value as 16 bit binary and display the value in final output
-        int [] finalOutput = new int [indexMerge];
-        for (int x = 0; x <= indexMerge - 1; x ++) {
-            int conversionIndex = 1;
-            for (int i = 15; i >= 0; i--) {
-                try {
-                    if (finalList.get(0) == 1) {
-                        finalOutput[x] = finalOutput[x] + conversionIndex;
-                    }
-                } catch (Exception e) {}
-                finalList.remove(0);    
-                conversionIndex = conversionIndex * 2;
-            }
-        }
-        
-        //Get the largest merged protfolio
-        Arrays.sort(finalOutput);
-        return finalOutput[indexMerge - 1];
-    }
-    
-    public static int[][] toBinary(int[] portfolios) {
         int [][] finalPortfolio = new int [portfolios.length][16];
         for (int x = 0; x <= portfolios.length - 1; x++) {
             int conversionIndex = 32768;
@@ -52,17 +18,42 @@ public class Question1 {
             }
         }
         
-        return finalPortfolio;
-    }
-    
-    public static List<Integer>  mergePortfolios(int[] portfolioA, int[] portfolioB, List<Integer> binaryList) {
-        for (int i = 0; i <= 15 ; i++) {
-            if ((portfolioA[i] == portfolioB[i]) && ((portfolioA[i] == 1) | (portfolioB[i] == 0))){
-                binaryList.add(0);
-            } else {
-                binaryList.add(1);
+        // Merge all the portfolios with another
+        List<Integer> binaryList = new ArrayList();
+        List<Integer> finalList = new ArrayList();
+        for (int z = 0; z <= portfolios.length - 1; z++) {
+            for (int y = portfolios.length - 1; y >= 0; y--) {
+                for (int i = 0; i <= 15 ; i++) {
+                    if ((finalPortfolio[z][i] == finalPortfolio[y][i]) && ((finalPortfolio[z][i] == 1) | (finalPortfolio[y][i] == 0))){
+                        binaryList.add(0);
+                    } else {
+                        binaryList.add(1);
+                    }
+                }
+                binaryList.clear();
             }
         }
-        return binaryList;
+        int indexMerge = (int) Math.sqrt(portfolios.length - 1);
+        
+        // Modify the finalList to return every value as 16 bit binary and display the value in final output
+        int findingLargest = 0;
+        int largest = 0;
+        for (int x = 0; x <= indexMerge - 1; x ++) {
+            int conversionIndex = 1;
+            for (int i = 15; i >= 0; i--) {
+                try {
+                    if (finalList.get(0) == 1) {
+                        findingLargest += conversionIndex;
+                    }
+                } catch (Exception e) {}
+                finalList.remove(0);    
+                conversionIndex = conversionIndex * 2;
+            }
+            if (findingLargest > largest)
+              largest = findingLargest;
+            findingLargest = 0;
+        }
+      
+        return largest;
     }
 }
