ab42a37ee979f55a6f52a8561c82fd8a918e2f3a
Thomas Haywood <thomashaywoodproductions@gmail.com>
2018-10-21 13:36:45 +0100

    Q4

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -3,8 +3,24 @@ package answers;
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
+        int total = 200000000;
+        int subTotal = 0;
+        for (int row = 0; row < rows.length; row++)
+            for (int sub = 0; sub < rows[row].length - numberMachines; sub++) {
+                subloop :
+                for (int m = sub; m < sub + numberMachines; m++) {
+                    if (rows[row][m] != "X") {
+                        subTotal += Integer.parseInt(rows[row][m]);
+                    } else {
+                        subTotal = -1;
+                        break subloop;
+                    }
+                }       
+                if (subTotal < total && subTotal != -1) 
+                    total = subTotal;
+                subTotal = 0;
+            }
+        return total != 200000000 ? total : 0;
+    }
 
 }
