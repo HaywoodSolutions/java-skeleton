f46db86062625e9e75c46847219cb3eaa85a765c
Unknown <thomashaywoodproductions@gmail.com>
2018-10-22 00:50:23 +0100

    Fixed Q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class Question1 {
     
     public static int bestMergedPortfolio(int[] portfolios) {
-        ArrayList<Integer> FinalPortfolio = new ArrayList<Integer>();
+        ArrayList<Integer> finalPortfolio = new ArrayList<Integer>();
         for(int p1=0; p1<portfolios.length; p1++) {
            for(int p2=p1+1; p2<portfolios.length; p1++) {
-               FinalPortfolio.add(portfolios[p1]^portfolios[p2]);
+               finalPortfolio.add(portfolios[p1]^portfolios[p2]);
            }
         }
         
