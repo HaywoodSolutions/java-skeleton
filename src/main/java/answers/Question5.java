f8a2d2531acf0bc2f356fd4e68bf6cc8ff23e89b
Unknown <thomashaywoodproductions@gmail.com>
2018-10-28 18:37:43 +0000

    Q5 Rewritten

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,37 +1,32 @@
 package answers;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Question5 {
+    public static int shareExchange(int[] allowedAllocations, int totalValue) {
+        Map<Integer, Integer> map = new HashMap<>();
+        return findMin(totalValue, allowedAllocations, map);
+    }
+    
+    public static int findMin(int total, int coins[], Map<Integer, Integer> map) {
+        if (total == 0)
+            return 0;
 
-	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		ArrayList<Integer> changeList = new ArrayList<Integer>();
-        for (int change : allowedAllocations) {
-            if (change == totalValue) {
-                return 1;
-            } else if (totalValue > change)
-                changeList.add(change);
+        if (map.containsKey(total))
+            return map.get(total);
+
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i < coins.length; i++) {
+            if(coins[i] > total)
+                continue;
+            int val = findMin(total - coins[i], coins, map);
+            if( val < min )
+                min = val;
         }
-        changeList.sort(Collections.reverseOrder());
-        
-        ArrayList<Integer> nodes = new ArrayList(changeList);
-        int pass = 1;
-        int total;
-        do {
-            pass++;
-            for (int n = 0; n < nodes.size(); n++)
-                for (int c = 0; c < changeList.size(); c++) {
-                    total = nodes.get(n) + changeList.get(c);
-                    if (total == totalValue) {
-                        return pass;
-                    } else if (total < totalValue) {
-                        nodes.add(total);
-                    }                
-                    nodes.remove(n);
-                }
-        } while (nodes.size() > 0);
-        return -1;
-	}
 
+        min =  (min == Integer.MAX_VALUE ? min : min + 1);
+        map.put(total, min);
+        return min;
+    }
 }
