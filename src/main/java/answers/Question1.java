b9359bc05999c81cbc7714ef73551b292f53d94a
Unknown <thomashaywoodproductions@gmail.com>
2018-10-28 23:47:05 +0000

    Q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,47 +1,25 @@
 package answers;
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Question1 {
     
    public static int bestMergedPortfolio(int[] portfolios) {
-        int[][] finalPortfolio = new int[portfolios.length][32];
-        
-        List<Integer> binaryList = new ArrayList();
-        List<Integer> finalList = new ArrayList();
-        for (int z = 0; z <= portfolios.length - 1; z++) {
-            for (int y = portfolios.length - 1; y >= 0; y--) {
-                for (int i = 0; i <= 31 ; i++) {
-                    if ((finalPortfolio[z][i] == finalPortfolio[y][i]) && ((finalPortfolio[z][i] == 1) || (finalPortfolio[y][i] == 0))){
-                        binaryList.add(0);
-                    } else {
-                        binaryList.add(1);
-                    }
+        String[] binaryPortfolios = new String[portfolios.length];
+        for (int z = 0; z < portfolios.length ; z++)
+            binaryPortfolios[z] = String.format("%016d", Integer.toString(portfolios[z], 2));
+
+        int currentTotal = Integer.MAX_VALUE;
+        int total;
+        for (int z = 0; z < binaryPortfolios.length ; z++) {
+            for (int y = z; y < binaryPortfolios.length; y++) {
+                total = 0;
+                for (int i = 0; i < 16; i++) {
+                    if (binaryPortfolios[z].charAt(i) != binaryPortfolios[y].charAt(i))
+                        total += (16 - i) * (16 - i);
                 }
-                binaryList.clear();
+                if (total > currentTotal)
+                    currentTotal = total;
             }
         }
-        int indexMerge = (int) Math.sqrt(portfolios.length - 1);
-        
-        int findingLargest = 0;
-        int largest = 0;
-        for (int x = 0; x <= indexMerge - 1; x ++) {
-            int conversionIndex = 1;
-            for (int i = 15; i >= 0; i--) {
-                try {
-                    if (finalList.get(0) == 1) {
-                        findingLargest += conversionIndex;
-                    }
-                } catch (Exception e) {}
-                finalList.remove(0);    
-                conversionIndex = conversionIndex * 2;
-            }
-            if (findingLargest > largest)
-              largest = findingLargest;
-            findingLargest = 0;
-        }
-      
-        return largest;
+        return currentTotal;
     }
 }
