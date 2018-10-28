b8b933f2008a808be60a37ad82c76503f749c26f
Unknown <thomashaywoodproductions@gmail.com>
2018-10-28 15:24:51 +0000

    Q6

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -18,23 +18,16 @@ public class Question5 {
         ArrayList<Integer> nodes = new ArrayList(changeList);
         int pass = 1;
         int total;
-        System.out.println(changeList);
         do {
             pass++;
             for (int n = 0; n < nodes.size(); n++)
                 for (int c = 0; c < changeList.size(); c++) {
                     total = nodes.get(n) + changeList.get(c);
-                    System.out.println(nodes.get(n) + " " + changeList.get(c) + " " + total +" "+ totalValue + " " + nodes.size());
                     if (total == totalValue) {
-                        System.out.println("EQUALS");
                         return pass;
                     } else if (total < totalValue) {
-                        System.out.println("LESS");
                         nodes.add(total);
-                    } else  {
-                        System.out.println("END NODE");
-                    }
-                
+                    }                
                     nodes.remove(n);
                 }
         } while (nodes.size() > 0);
