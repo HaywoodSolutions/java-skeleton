d834df4ff1981fc342ead605fb3a036fe310090e
Unknown <thomashaywoodproductions@gmail.com>
2018-10-30 16:24:59 +0000

    Q1 Cleanup

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,19 +3,19 @@ package answers;
 public class Question1 {
   
    public static int bestMergedPortfolio(int[] portfolios) {
-        if(portfolios.length < 2) {
-            return portfolios[0];
-        } else {
-            int n = (portfolios.length - 1);
-            int total;
-            int max = 0;
-            for (int i=0; i<n; i++)
-                for (int j=i+1; j<=n; j++) {
-                    total = (int) (portfolios[i] ^ portfolios[j]);              
-                    if (max<total) max = total;
-                }
-            return max;
+        //if(portfolios.length < 2) {
+        //    return portfolios[0];
+        int max = 0;
+        int total;
+
+        for (int i = 0; i < portfolios.length - 1; i++) {
+            for (int j = i + 1; j < portfolios.length; j++) {
+                total = portfolios[i] ^ portfolios[j];
+                if (total > max)
+                    max = total;
+            }
         }
+        return max;
     }
   
 }
