dd340c7aafdf4fa29c2a7a9603cb12f9ece98f06
Unknown <thomashaywoodproductions@gmail.com>
2018-10-30 14:46:36 +0000

    Q6

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -5,24 +5,35 @@ import java.util.Arrays;
 
 public class Question6 {
     public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
-        int startID = 0;
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        queue.add(startID);
+        int noServers = numServers;
+        int node = 0;
+        int[] result = new int[noServers];
         
-        int[] distances = new int[numServers];
-        Arrays.fill(distances, -1);
-        distances[startID] = 0;
-        
-        while(!queue.isEmpty()) {
-            int node = queue.poll();
-            for (int neighborID = 0; neighborID < times[node].length; neighborID++) {
-                if (distances[neighborID] == -1 || distances[node] + times[node][neighborID] < distances[neighborID]) {
-                    distances[neighborID] = distances[node] + times[node][neighborID];
-                    queue.add(neighborID);
-                }
-            }
+        for (int i=1; i<noServers; i++) {
+            int dijk_node = 0;
+            int minTime = -1;
+            for (int j=0; j<noServers; j++)
+                if (times[node][j] != -1 && node != j)
+                    if (minTime == -1) {
+                        minTime = times[node][j];
+                        dijk_node = j;
+                    } else if (minTime > times[node][j]) {
+                        minTime = times[node][j];
+                        dijk_node = j;
+                    }
+            
+            result[dijk_node] = minTime;
+            
+            if(dijk_node == targetServer)
+                break;
+                
+            times[node][dijk_node] = -1;
+            
+            for (int j=0; j<noServers; j++)
+                if (node != j && times[node][j] != -1)
+                    if (minTime + times[dijk_node][j] < times[node][j])
+                        times[node][j] = minTime + times[dijk_node][j];
         }
-        
-        return distances[targetServer];
+        return result[targetServer];
     }
 }
