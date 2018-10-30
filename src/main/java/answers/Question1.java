a7409507ef0cb7cd4c12c37e1d8ba9a25e55caaf
Unknown <thomashaywoodproductions@gmail.com>
2018-10-30 14:51:47 +0000

    Q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,15 +3,19 @@ package answers;
 public class Question1 {
   
    public static int bestMergedPortfolio(int[] portfolios) {
-        int n = (portfolios.length - 1);
-        int total;
-        int max = 0;
-        for (int i=0; i<n; i++)
-            for (int j=i+1; j<=n; j++) {
-                total = (int) (portfolios[i] ^ portfolios[j]);              
-                if (max<total) max = total;
-            }
-        return max;
+        if(portfolios.length < 2) {
+            return portfolios[0];
+        } else {
+            int n = (portfolios.length - 1);
+            int total;
+            int max = 0;
+            for (int i=0; i<n; i++)
+                for (int j=i+1; j<=n; j++) {
+                    total = (int) (portfolios[i] ^ portfolios[j]);              
+                    if (max<total) max = total;
+                }
+            return max;
+        }
     }
   
 }
