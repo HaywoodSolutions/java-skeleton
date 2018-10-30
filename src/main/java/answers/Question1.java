c0dbb39f6dc9c463c206f639aebe7e72c2b0e412
Unknown <thomashaywoodproductions@gmail.com>
2018-10-30 21:42:30 +0000

    q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -5,9 +5,10 @@ public class Question1 {
    public static int bestMergedPortfolio(int[] portfolios) {
         int max = 0;
         int total;
+        int n = portfolios.length;
 
-        for (int i = 0; i < portfolios.length - 1; i++)
-            for (int j = i + 1; j < portfolios.length; j++) {
+        for (int i = 0; i < n - 1; i++)
+            for (int j = i + 1; j < n; j++) {
                 total = portfolios[i] ^ portfolios[j];
                 if (total > max)
                     max = total;
