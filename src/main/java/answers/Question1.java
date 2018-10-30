35d52f1bc8a6d0fae1ea7251d6ea24bdbef1fc7a
Unknown <thomashaywoodproductions@gmail.com>
2018-10-30 14:31:48 +0000

    Q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,29 +1,17 @@
 package answers;
 
-import java.util.Arrays;
-
 public class Question1 {
   
    public static int bestMergedPortfolio(int[] portfolios) {
-         int currentTotal = 0;
+        int n = (portfolios.length - 1);
         int total;
-        //String binaryTotal = "0000000000000000";
-        //String binary;
-        for (int z = 0; z < portfolios.length ; z++)
-            for (int y = z; y < portfolios.length; y++) {
-                total = portfolios[z] ^ portfolios[y];
-                total = total % 65536;
-                
-                
-                //binary = String.format("%16s", Integer.toString(total, 2).toString()).replace(' ', '0');
-                //if (binary.length() > 16)
-                //    binary.substring(binary.length() - 1 - 16, binary.length());
-                if (total > currentTotal) {
-                    currentTotal = total;
-                    //binaryTotal = binary;
-                }
+        int max = 0;
+        for (int i=0; i<n; i++)
+            for (int j=i+1; j<=n; j++) {
+                total = (int) (portfolios[i] ^ portfolios[y]);              
+                if (max<total) max = total;
             }
-        return currentTotal;
+        return max;
     }
   
 }
